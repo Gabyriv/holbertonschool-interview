fef212d0565a9264f1aede68d801ce7604990ff4
Gabriel <gabyrivm06@gmail.com>
2025-04-02 11:59:02 -0400

    Added the solution to the Lockboxes task. Used the Breath-First Search(BFS) this explores all neighbors at the current depths before moving to nodes at the next depth level and it uses a queue data structure

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -9,16 +9,34 @@ Problem:
 Task:
     Write a method that determines if all the boxes can be opened.
 
-    Prototype: def canUnlockAll(boxes)
-    boxes is a list of lists
-    A key with the same number as a box opens that box
-    You can assume all keys will be positive integers
-        There can be keys that do not have boxes
-    The first box boxes[0] is unlocked
-    Return True if all boxes can be opened, else return False
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
 """
 
 from typing import List
+from collections import deque
 
 
 def canUnlockAll(boxes: List[List[int]]) -> bool:
+    # If boxes is empty I return False because
+    # I can't open any boxes
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = deque(boxes[0])
+
+    while queue:
+        key = queue.popleft()
+        if 0 <= key < n and not visited[key]:
+            visited[key] = True
+            queue.extend(boxes[key])
+
+    return all(visited)
